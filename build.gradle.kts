1e4c6f4c25255ee56f1e3a46ca2fcb821720ab00
Baron Roberts <baron@cthing.com>
2024-04-14 14:54:15 -0700

    Add GitHub CI and sponsor files.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,8 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-allprojects {
-    repositories {
-        mavenCentral()
-    }
+repositories {
+    mavenCentral()
 }
 
 plugins {
